2100d4ec8e8c721c1970b9148aae5455ad1d223c
Yash Khatri <ykhatri96@gmail.com>
2024-10-21 16:41:28 +0530

    Updated unit tests and method signature to the correct one

--- string_calculator.py
+++ string_calculator.py
@@ -2,18 +2,17 @@ import re
 
 
 class StringCalculator:
-    def __init__(self, numbers: str):
-        self.numbers = numbers
-
-    def get_delimiters(self) -> str:
+    @staticmethod
+    def get_delimiters(numbers: str) -> tuple:
         """
         Extract delimiters from the given string
+        :param: numbers:
         :return: delimiter string
         """
         # Check if custom delimiter is present
-        if self.numbers.startswith('//'):
+        if numbers.startswith('//'):
             # Split into delimiter and numbers
-            delimiter_part, numbers_part = self.numbers.split('\n', 1)
+            delimiter_part, numbers_part = numbers.split('\n', 1)
 
             # Extract delimiters, supporting multiple delimiters of any length
             custom_delimiters = re.findall(r'\[(.*?)\]', delimiter_part)
@@ -24,27 +23,28 @@ class StringCalculator:
 
             # Join delimiters and add escape character to avoid matching regex issue
             delimiter = '|'.join(map(re.escape, custom_delimiters))
-            self.numbers = numbers_part  # The rest of the numbers remain unchanged
+            numbers = numbers_part  # The rest of the numbers remain unchanged
         else:
             # Default delimiter is a comma
             delimiter = ','
-        return delimiter
+        return delimiter, numbers
 
-    def add(self) -> int:
+    def add(self, numbers: str) -> int:
         """
         Adds all given numbers
+        :param: numbers:
         :return: total of given numbers
         """
-        if not self.numbers:
+        if not numbers:
             return 0
 
-        delimiter = self.get_delimiters()
+        delimiter, cleaned_numbers = self.get_delimiters(numbers)
 
         # Replace newline with comma
-        self.numbers = self.numbers.replace('\n', ',')
+        cleaned_numbers = cleaned_numbers.replace('\n', ',')
 
         # Split the string using regex for multiple delimiters
-        nums_list = [int(num) for num in re.split(delimiter, self.numbers) if num]
+        nums_list = [int(num) for num in re.split(delimiter, cleaned_numbers) if num]
 
         # raise value error for negative numbers
         negative_nums = ', '.join([str(num) for num in nums_list if num < 0])
@@ -60,4 +60,4 @@ class StringCalculator:
 if __name__ == '__main__':
     numbers_list = ["1\n2,3", "//;\n1;2", "//[***]\n1***2***3", "//[*][%]\n1*2%3", "1001,2", "-11,2,-100"]
     for numbers in numbers_list:
-        print(StringCalculator(numbers).add())
+        print(StringCalculator().add(numbers))
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -5,51 +5,50 @@ from string_calculator import StringCalculator
 class TestStringCalculator(unittest.TestCase):
 
     def test_empty_string(self):
-        self.assertEqual(StringCalculator("").add(), 0)
+        self.assertEqual(StringCalculator().add(""), 0)
 
     def test_single_number(self):
-        self.assertEqual(StringCalculator("100").add(), 100)
+        self.assertEqual(StringCalculator().add("100"), 100)
 
     def test_multiple_numbers(self):
-        self.assertEqual(StringCalculator("23,45").add(), 68)
-        self.assertEqual(StringCalculator("23,21,98,45,34").add(), 221)
-        self.assertEqual(StringCalculator("210,29,38").add(), 277)
+        self.assertEqual(StringCalculator().add("23,45"), 68)
+        self.assertEqual(StringCalculator().add("23,21,98,45,34"), 221)
+        self.assertEqual(StringCalculator().add("210,29,38"), 277)
 
     def test_newline_delimiter(self):
-        self.assertEqual(StringCalculator("11\n12,13").add(), 36)
-        self.assertEqual(StringCalculator("11\n12\n13").add(), 36)
+        self.assertEqual(StringCalculator().add("11\n12,13"), 36)
+        self.assertEqual(StringCalculator().add("11\n12\n13"), 36)
 
     def test_custom_delimiter(self):
         # Custom delimiter ";"
-        self.assertEqual(StringCalculator("//;\n1;2").add(), 3)
+        self.assertEqual(StringCalculator().add("//;\n1;2"), 3)
         # Custom delimiter "!"
-        self.assertEqual(StringCalculator("//!\n4!10!23").add(), 37)
+        self.assertEqual(StringCalculator().add("//!\n4!10!23"), 37)
         # Custom delimiter "|"
-        self.assertEqual(StringCalculator("//|\n2|3|4").add(), 9)
+        self.assertEqual(StringCalculator().add("//|\n2|3|4"), 9)
 
     def test_multiple_delimiters(self):
         # Multiple delimiters "*", "%"
-        self.assertEqual(StringCalculator("//[*][%]\n1*2%3").add(), 6)
+        self.assertEqual(StringCalculator().add("//[*][%]\n1*2%3"), 6)
         # Multiple delimiters "!", "@"
-        self.assertEqual(StringCalculator("//[!][@]\n1!2@3").add(), 6)
+        self.assertEqual(StringCalculator().add("//[!][@]\n1!2@3"), 6)
         # Multiple delimiters "***", "@@@"
-        self.assertEqual(StringCalculator("//[***][@@@]\n1***2@@@3").add(), 6)
+        self.assertEqual(StringCalculator().add("//[***][@@@]\n1***2@@@3"), 6)
 
     def test_long_multiple_delimiters(self):
         # Multiple long delimiters "$$", "^^"
-        self.assertEqual(StringCalculator("//[$$][^^]\n1$$2^^3").add(), 6)
+        self.assertEqual(StringCalculator().add("//[$$][^^]\n1$$2^^3"), 6)
 
     def test_negative_numbers(self):
         with self.assertRaises(ValueError) as context:
-            StringCalculator("-1,-2,3").add()
+            StringCalculator().add("-1,-2,3")
         self.assertEqual(str(context.exception), "negative numbers not allowed: -1, -2")
 
     # Test for ignoring large numbers
     def test_ignore_large_numbers(self):
-        calc1 = StringCalculator("2,2001")
-        self.assertEqual(calc1.add(), 2)  # Should ignore 1001
-        calc2 = StringCalculator("1000,3011,2020,5")
-        self.assertEqual(calc2.add(), 1005)  # Only 1000 and 5 should be summed
+        calc = StringCalculator()
+        self.assertEqual(calc.add("2,2001"), 2)  # Should ignore 2001
+        self.assertEqual(calc.add("1000,3011,2020,5"), 1005)  # Only 1000 and 5 should be summed
 
 
 if __name__ == '__main__':
