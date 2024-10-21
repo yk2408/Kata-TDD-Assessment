df731c2288f83c1803e861426efd5899d3a99b52
Yash Khatri <ykhatri96@gmail.com>
2024-10-21 14:09:20 +0530

    Added class and methods boilerplate

---
+++ string_calculator.py
@@ -0,0 +1,16 @@
+class StringCalculator:
+    def get_delimiters(self, numbers: str) -> str:
+        """
+        Extract delimiters from the given string
+        :param numbers: string with delimiters and actual numbers to be added
+        :return: delimiter string
+        """
+        pass
+
+    def add(self, numbers: str) -> int:
+        """
+        Adds all given numbers
+        :param numbers: string with delimiters and actual numbers to be added
+        :return: total of given numbers
+        """
+        pass
